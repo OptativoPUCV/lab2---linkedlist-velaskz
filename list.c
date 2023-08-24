14ea8e2af7c71e118be933daaf52bee5b156f2e2
Replit user <>
2023-08-24 20:25:37 +0000

    new try

--- list.c
+++ list.c
@@ -106,6 +106,7 @@ void pushCurrent(List * list, void * data) {
   else{
     Node * nuevoNodo = createNode(data);
     nuevoNodo->prev = list->head;
+    list->tail = nuevoNodo;
     list->current->next = nuevoNodo;
   }
 }
